6037bcc14fd9084594edaf8a54c5a108ba4c8a6d
Jagan-Konda <jagankonda83@gmail.com>
2025-02-17 20:11:01 +0530

    second commit

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,3 +1,7 @@
-const NotFound = () => (<p>NotFound</p>)
+import './index.css'
+
+const NotFound = () => (<div className='page-not-found-container'>
+    <h1 className="page-not-found">Page Not Found</h1>
+</div>)
 
 export default NotFound
